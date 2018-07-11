783cab7ecb657fcea513f281e719589d67ccaf8f
Aaron Pecson <aspecson@gmail.com>
2018-07-11 14:55:32 -0700

    Playing with static paths

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 const withSass = require('@zeit/next-sass')
 module.exports = withSass()
 module.exports.exportPathMap = function (defaultPathMap) {
-     return {
-         '/': { page: '/' }
-     }
- }
+    return {
+        '/': { page: '/' },
+        '/newsfeed': { page: 'newsfeed' }
+    }
+}
