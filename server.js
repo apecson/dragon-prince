0631abee4139ab91206dbc58e6be1f4a4ba4e734
Aaron Pecson <aspecson@gmail.com>
2018-07-13 13:12:00 -0700

    Whisper

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+var wwwhisper = require('connect-wwwhisper');
+
+app.use(wwwhisper());
 app.prepare()
     .then(() => {
         const server = express()
