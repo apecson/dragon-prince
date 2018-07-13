525d689a86bd08bf3ab686e7fd45426a5e26a576
Aaron Pecson <aspecson@gmail.com>
2018-07-12 21:24:05 -0700

    Static and footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,21 @@
+import Link from 'next/link';
+
 const Footer = () => (
     <footer className="pt-4 pb-4">
         <div className="container text-center">
             &copy; Copyright Wonderstorm
         </div>
+        <ul class="d-flex justify-content-center nav">
+            <li class="nav-item">
+                <Link href=""><a class="text-white nav-link active">Terms of Service</a></Link>
+            </li>
+            <li class="nav-item">
+                <Link href=""><a class="text-white nav-link" >Privacy Policy</a></Link>
+            </li>
+            <li class="nav-item">
+                <Link href=""><a class="text-white nav-link">Cookie Policy</a></Link>
+            </li>
+        </ul>
     </footer>
 )
 
---
+++ pages/privacy-policy.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { createClient } from 'contentful'
+import Layout from '../components/MyLayout';
+import { markdown } from 'markdown';
+import renderHTML from 'react-render-html';
+
+
+const client = createClient({
+    space: "sykm2zb64bkw",
+    accessToken: "9424211d562951847401a3cbf1ab7bd6c266a6b20c7b68f7500e8b1de8fc1e14"
+});
+
+class Policy extends Component {
+
+    static async getInitialProps() {
+        // Get Data
+        const policy = await client.getEntry('2rlWNDRgIoiEmSa8AqEcOG');
+
+        return {
+            policy
+        }
+    }
+
+
+    render() {
+        console.log(this.props.policy);
+        return (
+            <Layout>
+                <div className="container pt-4 pb-4">
+                    <h1 className="page-title">{this.props.policy.fields.title}</h1>
+                    <div>
+                        {renderHTML(markdown.toHTML(this.props.policy.fields.body))}
+                    </div>
+                </div>
+            </Layout>
+        )
+    }
+}
+export default Policy
