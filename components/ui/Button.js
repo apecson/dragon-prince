43ba71be97a23edcc49bca248c81694254786828
Richard Rosales <rrosales@eharmony.com>
2018-07-17 12:54:57 -0700

    homepage fixed

--- components/ui/Button.js
+++ components/ui/Button.js
@@ -7,12 +7,17 @@ export default class Button extends Component {
     render() {
         const Button = styled.button`
             background: #4A90E2;
-            height: ${this.props.size === 'large' ? '50px' : '25px'};
+            height: auto;
+            min-height: ${this.props.size === 'large' ? '50px' : '25px'};
             color: #fff;
             border: 1px solid #979797;
             border-radius: 5px;
             padding: 0 20px;
             margin: 0 7px;
+            @media (max-width: 748px) {
+                height: auto;
+                min-height: 50px;
+            } 
         `
 
         return (
